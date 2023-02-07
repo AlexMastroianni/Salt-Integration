08d22d61d7b90a780df275e5247b420826b8cef3
AlexMastroianni <alex.m.mastroianni@gmail.com>
2023-02-08 08:53:59 +1100

    added new pages and calednely intergration

---
+++ src/components/Blurb/blurb.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function blurb() {
+  return (
+    <div>
+      <h2 className="blurbHeader">
+        Have the infomation you need to start your next smart home project
+      </h2>
+      <p>
+        With over 7 years experiance both installing and programming all smart
+        home techonogly professially we will be able to help
+      </p>
+    </div>
+  );
+}
+
+export default blurb;
